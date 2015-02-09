fb7c9375cef404958dcca3a5be2234584cf4b27a
ariel <ariel_556@hotmail.com>
2015-02-09 14:09:44 -0700

    first commit

---
+++ seriousApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  seriousApp
+//
+//  Created by Ariel Robles on 2/9/15.
+//  Copyright (c) 2015 nearsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ seriousApp/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  seriousApp
+//
+//  Created by Ariel Robles on 2/9/15.
+//  Copyright (c) 2015 nearsoft. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "AFNetworking.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
